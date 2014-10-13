f3dd18dc0124ec9c3f720ff430db79976e58de2b
zhangpan <zp@yunshipei.com>
2014-10-13 18:55:46 +0800

    first commit

--- js/background.js
+++ js/background.js
@@ -1,51 +1,87 @@
- var engine = ["sogou", "baidu", "google"];
- var defaultObj = {
- 	"copy": true,
- 	"plainCopy": false,
- 	"search": true,
- 	"engine": engine[0],
- 	"translate": true,
- 	"disableAll": false
- };
-var optionsObj = localStorage.options?JSON.parse(localStorage.options):defaultObj;
-chrome.extension.onRequest.addListener(function(msg) {
-	switch (msg.cmd) {
-		case 'copy':
-			var input = document.createElement('textarea');
-			document.body.appendChild(input);
-			input.value = msg.text;
-			input.focus();
-			input.select();
-			var a = document.execCommand('Copy');
-			input.remove();
-			break;
-		case 'translate':
-			var xhp = new XMLHttpRequest();
-			xhp.onreadystatechange = function() {
-				if (xhp.readyState == 4 && xhp.status == 200) { //xhp.responseText;
-					chrome.tabs.query({
-						active: true
-					}, function(tabs) {
-						chrome.tabs.sendMessage(tabs[0].id, {
-							cmd: 'translate-back',
-							data: xhp.responseText
-						});
-					});
-				}
-			}
-			xhp.open('GET', 'http://fanyi.youdao.com/openapi.do?keyfrom=sogou-inc&key=1309482814&type=data&doctype=json&version=1.1&q=' + msg.text, true);
-			xhp.send();
-		case 'getOptions':
-			chrome.tabs.query({
-				active: true
-			}, function(tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, {
-					cmd: 'options-back',
-					data: optionsObj
-				});
-			});
-			break;
-		default:
-			return;
+function sendRequest(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4 && xhr.status === 200) {
+			callback(xhr.responseText);
+		}
 	}
-});
+	xhr.open('GET', url, true);
+	xhr.send();
+}
+
+function $(selector) {
+	return document.getElementById(selector);
+}
+
+var g_config = (new Function('return ' + document.getElementsByTagName('title')[0].nextElementSibling.textContent.replace(/\g_config.dynamicScript.*/g, '')))();//大部分参数
+//店铺信息url
+var shopInfoSource = $('J_ShopInfo').getAttribute('data-rateUrl');//获取店铺信息链接
+var microscopeData = document.getElementsByName('microscope-data')[0].getAttribute('content').split(';').slice(0, -1);//必要参数
+var objBase = {};
+// microscopeData.forEach(function(item) {
+// 	var sub = item.split('=');
+// 	objBase[sub[0]] = sub[1];
+// });
+
+(function(){
+	var sub;
+	for(var i = 0; i< microscopeData.length; i++) {
+		sub = item.split('=');
+		objBase[sub[0]] = sub[1];
+	}
+})();
+
+//评价参数
+var commonOptions = {
+	callback: 'jsonp_reviews_list',
+	userNumId: g_config.sellerId,
+	auctionNumId: g_config.pageId || g_config.itemId,
+	siteID: objBase.siteId,
+	currentPageNum: 1,
+	rateType: -1,
+	orderType: 'sort_weight',
+	showContent: 1,
+	attribute: ''
+};
+
+//评价页码
+function commonPageNum(num) {
+	commonOptions.currentPageNum = num;
+}
+//店铺信息
+function getShopInfo() {
+	sendRequest(shopInfoSource, shopInfoBundle);
+}
+
+var shopDataDetail = {};
+function shopInfoBundle(data) {
+	//正则取出所有数据
+	var shopLocation = data.slice(data.indexOf("class=\"text\"", data.indexOf("<dt>所在地区：")) + 13, data.indexOf("</span", data.indexOf("<dt>所在地区：")));//所在地
+	var foundDate = data.slice(data.indexOf("class=\"text\"", data.indexOf("<dt>创建时间")) + 13, data.indexOf("</span", data.indexOf("<dt>创建时间")));//成立日期
+	//后加载数据链接
+	var lateDataMatch = new RegExp(/id="monthuserid"\svalue="(.*)(?=")/);//请求参数
+	var lateDataSource = 'http://rate.taobao.com/ShopService4C.htm?userNumId=' + lateDataMatch.exec(data)[1].trim() + '&shopID=' + g_config.shopId + '&isB2C=false';
+	sendRequest(lateDataSource, function(data) {
+		shopDataDetail = data;
+		data.complaints.indVal//纠纷
+		data.punish.indVal//处罚
+		data.ratRefund.indVal//退款
+	});
+}
+
+//评价url拼接
+function rateUrl() {
+	return http: //rate.taobao.com/feedRateList.htm?callback=jsonp_reviews_list&userNumId=shopInfoSource.
+}
+
+//评价
+function sellRecord() {
+	//评价数量
+	var commonInfoSource = document.getElementById('reviews').getAttribute('data-commonApi');
+	sendRequest(commonInfoSource, );//处理评价比率
+	//差评汇总
+	var badCommonInfoSource = 'http://rate.taobao.com/feedRateList.htm?callback=jsonp_reviews_list&userNumId=' + commonOptions.userNumId + '&auctionNumId=' + commonOptions.auctionNumId + '&siteId=' + commonOptions.siteId + '&currentPageNum=' + commonOptions.currentPageNum + '&rateType=-1&orderType=sort_weight&showContent=1&attibute=';
+	sendRequest(badCommonInfoSource, function() {
+
+	})
+}
